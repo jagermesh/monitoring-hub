c55e934b65190aa02fcea94eedf955c8a52f5a2f
Sergiy Lavryk <jagermesh@gmail.com>
2020-09-25 15:16:08 +0300

    fx

--- index.js
+++ index.js
@@ -27,11 +27,8 @@ module.exports = function(config) {
 
   _this.start = function() {
 
-    const observers       = [];
-    const sensors         = [];
-    const sensorDataCache = [];
-
-    const clients = {};
+    const observers = [];
+    const sensors   = [];
 
     // hub server
 
@@ -53,14 +50,12 @@ module.exports = function(config) {
         socket.emit('sensorRegistered', { sensorInfo: sensorInfo });
         _this.log('Sensor registered', { sensorUid: data.sensorUid });
 
-        setTimeout(function() {
-          _this.log('Sending sensor info to observers', { sensorUid: sensorInfo.sensorUid });
-          for(let observerId in observers) {
-            let observer = observers[observerId];
-            _this.log('Sending sensor info to observer', { sensorUid: sensorInfo.sensorUid, observerId: observer.observerInfo.observerId });
-            observer.socket.emit('sensorRegistered', { sensorInfo: sensorInfo });
-          }
-        });
+        _this.log('Sending sensor info to observers', { sensorUid: sensorInfo.sensorUid });
+        for(let observerId in observers) {
+          let observer = observers[observerId];
+          _this.log('Sending sensor info to observer', { sensorUid: sensorInfo.sensorUid, observerId: observer.observerInfo.observerId });
+          observer.socket.emit('sensorRegistered', { sensorInfo: sensorInfo });
+        }
       });
       socket.on('registerObserver', function(data) {
         let observerInfo = Object.assign({ observerId: connectionInfo.id }, connectionInfo);
@@ -69,52 +64,51 @@ module.exports = function(config) {
         socket.emit('observerRegistered', { observerInfo: observerInfo });
         _this.log('Observer registered', { observerId: observerInfo.id });
 
-        setTimeout(function() {
-          _this.log('Sending sensors list to observers');
-          for (let sensorId in sensors) {
-            let sensor = sensors[sensorId];
-            _this.log('Sending sensor info to observer',  { sensorUid: sensor.sensorInfo.sensorUid, observerId: observerInfo.observerId });
-            socket.emit('sensorRegistered', { sensorInfo: sensor.sensorInfo });
+        _this.log('Sending sensors list to observers');
+        for (let sensorId in sensors) {
+          let sensor = sensors[sensorId];
+          _this.log('Sending sensor info to observer',  { sensorUid: sensor.sensorInfo.sensorUid, observerId: observerInfo.observerId });
+          socket.emit('sensorRegistered', { sensorInfo: sensor.sensorInfo });
+        }
+        _this.log('Sending sensors data to observers');
+        for (let sensorId in sensors) {
+          let sensor = sensors[sensorId];
+          if (sensor.sensorData) {
+            _this.log('Sending sensor data to observer',  { sensorUid: sensor.sensorInfo.sensorUid, metricUid: sensor.sensorData.metricUid, observerId: observerInfo.observerId });
+            socket.emit('sensorData', sensor.sensorData);
           }
-          setTimeout(function() {
-            _this.log('Sending sensors data to observer');
-            for (let sensorId in sensors) {
-              let sensor = sensors[sensorId];
-              let sensorData = sensorDataCache[sensor.sensorInfo.sensorUid];
-              if (sensorData) {
-                _this.log('Sending sensor data to observer',  { sensorUid: sensor.sensorInfo.sensorUid, metricUid: sensorData.metricUid, observerId: observerInfo.observerId });
-                socket.emit('sensorData', sensorData);
-              }
-            }
-          });
-        });
+        }
       });
       socket.on('sensorData', function(data) {
-        let sensorData = Object.assign({ }, data);
-        sensorDataCache[sensorData.sensorUid] = sensorData;
-        for (let observerId in observers) {
-          let observer = observers[observerId];
-          // _this.log('Sending sensor data to observer',  { sensorUid: sensorData.sensorUid, metricUid: sensorData.metricUid, observerId: observer.observerInfo.observerId });
-          observer.socket.emit('sensorData', sensorData);
+        let sensor = sensors[connectionInfo.id];
+        if (sensor) {
+          let sensorData = Object.assign({ }, data);
+          sensor.sensorData = sensorData;
+          for (let observerId in observers) {
+            let observer = observers[observerId];
+            // _this.log('Sending sensor data to observer',  { sensorUid: sensorData.sensorUid, metricUid: sensorData.metricUid, observerId: observer.observerInfo.observerId });
+            observer.socket.emit('sensorData', sensorData);
+          }
         }
       });
       socket.on('disconnect', function() {
+        _this.log('Disconnection', connectionInfo);
         let sensor = sensors[connectionInfo.id];
         delete sensors[connectionInfo.id];
-        delete sensorDataCache[sensor.sensorInfo.sensorUid];
         if (sensor) {
-          _this.log('Sensor disconnected', { sensorUid: sensor.sensorInfo.sensorUid });
+          _this.log('Disconnection of sensor', { sensorUid: sensor.sensorInfo.sensorUid });
+          _this.log('Informing observers about sensor disconnection',  { sensorUid: sensor.sensorInfo.sensorUid });
           for(let observerId in observers) {
             let observer = observers[observerId];
             _this.log('Informing observer about sensor disconnection',  { sensorUid: sensor.sensorInfo.sensorUid, observerId: observer.observerInfo.observerId });
-            observers[observerId].socket.emit('sensorUnregistered', { sensorInfo: sensor.sensorInfo });
+            observer.socket.emit('sensorUnregistered', { sensorInfo: sensor.sensorInfo });
           }
           _this.log('Sensor disconnected', { sensorUid: sensor.sensorInfo.sensorUid });
         }
         let observer = observers[connectionInfo.id];
         delete observers[connectionInfo.id];
         if (observer) {
-          _this.log('Observer disconnected', { observerId: observer.observerInfo.observerId });
+          _this.log('Disconnection of observer', { observerId: observer.observerInfo.observerId });
         }
       });
     });
